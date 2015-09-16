686b1efb9d892c99aaf74be1281318fc1450b3ff
Paula Vivian <paula.viviansilva@gmail.com>
2015-09-16 09:51:53 -0300

    Criação do projeto

---
+++ ifcontrol/src/nc/ifrn/Inicializacao.java
@@ -0,0 +1,16 @@
+package nc.ifrn;
+
+/**
+ * Classe responsável por iniciar o módulo desktop.
+ * 
+ * @author Paula Vivian
+ *
+ */
+public class Inicializacao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ifcontrol/src/nc/ifrn/apresentacao/TelaLogin.java
@@ -0,0 +1,5 @@
+package nc.ifrn.apresentacao;
+
+public class TelaLogin {
+
+}
